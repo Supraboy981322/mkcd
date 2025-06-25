626258e66a6f87065910b85c9e4bdc101b27f4e8
Supraboy981322 <97927259+Supraboy981322@users.noreply.github.com>
2025-06-25 14:17:04 -0500

    Added the progress numbers and fixed a bug

--- install.sh
+++ install.sh
@@ -5,27 +5,27 @@ echo -e "  \e[33mInstallation started...\e[0m"
 echo -e "\e[34mCreating mkcd install directory\e[0m"
 mkdir /etc/scripts
 
-echo -e "\e[36mProgress: ??%\e[0m"
+echo -e "\e[36mProgress: 20%\e[0m"
 
 echo -e "  \e[33mCreated mkcd install directory\e[0m"
 echo -e "\e[34mMoving to mkcd install directory\e[0m"
 cd /etc/scripts
 
-echo -e "\e[36mProgress: ??%\e[0m"
+echo -e "\e[36mProgress: 40%\e[0m"
 
 echo -e "  \e[33mMoved to mkcd install directory\e[0m"
 echo -e "\e[34mFetching mkcd\e[0m"
 echo -e "  \e[33m(please enter your sudo password so the install can create the script after fetching)\e[0m"
 sudo curl https://raw.githubusercontent.com/Supraboy981322/mkcd/refs/heads/main/src/mkcd -o mkcd
 
-echo -e "\e[36mProgress: ??%\e[0m"
+echo -e "\e[36mProgress: 60%\e[0m"
 
 echo -e "  \e[33mFetched mkcd\e[0m"
 echo -e "\e[34mMaking mkcd executable\e[34m"
 echo -e "  \e[33m(Your sudo password may be required once more to make it executable)\e[0m"
 sudo chmod +x mkcd
 
-echo -e "\e[36m Progress: ??%\e[0m"
+echo -e "\e[36mProgress: 80%\e[0m"
 
 echo -e "  \e[33mMade mkcd executable\e[0m"
 echo -e "\e[34mCreating alias\e[0m"
@@ -35,5 +35,4 @@ source ~/.bashrc
 echo -e "\e[36mProgress: 100%\e[0m"
 echo -e "  \e[33mCompleted!\e[33m"
 
-echo -e "\e[34mTesting mkcd\e[0m"
-mkcd -h
+echo -e "\e[34mSee\e[0m \e[32mmkcd -h\e[0m\e[34m for usage\e[0m"
